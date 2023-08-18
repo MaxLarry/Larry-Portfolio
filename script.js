7e9a9a0f6b74a1b41062e0de75a71941904ae983
YlarHeartbb <202080010@psu.palawan.ph>
2023-08-18 20:54:25 +0800

    first modified portfolio

--- script.js
+++ script.js
@@ -34,54 +34,6 @@ window.addEventListener('scroll', function () {
     closeDropDownMenu();
 });
 
-// about animation
-
-const aboutSection = document.querySelector('.ABOUT');
-const box = aboutSection.querySelector('.box');
-
-const options = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.2 // Adjust this value to control when the reveal effect is triggered
-};
-
-const revealCallback = function(entries, observer) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      box.classList.add('animate-reveal');
-      observer.unobserve(entry.target);
-    }
-  });
-};
-const observer = new IntersectionObserver(revealCallback, options);
-observer.observe(aboutSection);
-
-
-
-// portfolio animation
-
-const LATESTSection = document.querySelector('.LATEST');
-const slide = LATESTSection.querySelector('.slider');
-
-const optionsLATEST = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.4 // Adjust this value to control when the reveal effect is triggered
-};
-
-const revealCallbackLATEST = function(entries, observer) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      slide.classList.add('animate-reveal');
-      observer.unobserve(entry.target);
-    }
-  });
-};
-
-const observerLATEST = new IntersectionObserver(revealCallbackLATEST, optionsLATEST);
-observerLATEST.observe(LATESTSection);
-
-
 
 const ServiceSection = document.querySelector('.SERVICES');
 const cards = ServiceSection.querySelectorAll('.card1, .card2, .card3');
@@ -107,3 +59,42 @@ const observerService = new IntersectionObserver(revealCallbackService, optionsS
 observerService.observe(ServiceSection);
 
 
+////////////**********effects************///////////////
+
+const homet = document.querySelectorAll('.eff');
+
+function checkhomeVisibility() {
+  const threshold = 1; // Adjust this value as needed
+
+  homet.forEach(HOM => {
+    const hoe = HOM.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+
+    // Calculate the threshold offset based on the card's height
+    const thresholdOffset = hoe.height * (1 - threshold);
+
+    if (hoe.top < windowHeight - thresholdOffset && hoe.bottom >= 0) {
+        HOM.classList.add('animate-reveal');
+    }
+  });
+}
+
+// Initial check
+checkhomeVisibility();
+
+// Listen for scroll events
+window.addEventListener('scroll', checkhomeVisibility);
+
+
+
+////////////**********NAV************///////////////
+
+$(document).ready(function(){
+  $(window).on('scroll', function(){
+    if ($(window).scrollTop()) {
+              $("header").addClass('bgc');
+    }else{
+              $("header").removeClass('bgc');
+    }
+  });
+});
